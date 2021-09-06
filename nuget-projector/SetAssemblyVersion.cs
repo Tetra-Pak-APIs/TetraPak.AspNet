06ba6baaec29d0ec557494b53617e67b89f7c0ac
Jonas Rembratt <jonas.rembratt@tetrapak.com>
2021-09-06 11:31:51 +0200

    Removed experimental cmdlet project "nuget-projector"

--- nuget-projector/SetAssemblyVersion.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Management.Automation;
-
-namespace TetraPak.Tools
-{
-    [Cmdlet(VerbsCommon.Set, "AssemblyVersion")]
-    public class SetAssemblyVersion : PSCmdlet
-    {
-    }
-}
