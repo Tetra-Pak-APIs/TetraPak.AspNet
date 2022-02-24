334d45e0bf043e931ebd11f9746f989b999237e7
Jonas Rembratt <jonas.rembratt@tetrapak.com>
2022-02-24 13:21:25 +0100

    Improved service configuration

--- TetraPak.AspNet.Api/Auth/tokenExchange/TetraPakTokenExchangeHelper.cs
+++ TetraPak.AspNet.Api/Auth/tokenExchange/TetraPakTokenExchangeHelper.cs
@@ -4,22 +4,41 @@ using Microsoft.Extensions.DependencyInjection.Extensions;
 
 namespace TetraPak.AspNet.Api.Auth
 {
+    /// <summary>
+    ///   Provides convenient methods for working with OAuth grant flows. 
+    /// </summary>
     public static class TetraPakTokenExchangeHelper
     {
-        public static IServiceCollection AddTetraPakTokenExchangeService(this IServiceCollection c)
+        /// <summary>
+        ///   (fluent api)<br/>
+        ///   Adds a Tetra Pak service to support the OAuth Token Exchange Grant flow
+        ///   to the service collection and then returns the service collection.
+        /// </summary>
+        public static IServiceCollection AddTetraPakTokenExchangeService(this IServiceCollection collection)
         {
-            c.AddTetraPakConfiguration<TetraPakApiConfig>();
-            c.TryAddSingleton<ITokenExchangeGrantService, TetraPakTokenExchangeGrantService>();
-            return c;
+            collection.AddTetraPakConfiguration<TetraPakApiConfig>();
+            collection.AddAmbientData();
+            collection.AddTetraPakHttpClientProvider();
+            collection.TryAddSingleton<ITokenExchangeGrantService, TetraPakTokenExchangeGrantService>();
+            return collection;
         }
 
-        public static IServiceCollection AddTetraPakClientCredentialsService(this IServiceCollection c)
+        /// <summary>
+        ///   (fluent api)<br/>
+        ///   Adds a Tetra Pak service to support the OAuth Client Credentials Grant flow
+        ///   to the service collection and then returns the service collection.
+        /// </summary>
+        public static IServiceCollection AddTetraPakClientCredentialsService(this IServiceCollection collection)
         {
-            c.AddTetraPakConfiguration<TetraPakApiConfig>();
-            c.TryAddSingleton<IClientCredentialsGrantService, TetraPakClientCredentialsGrantService>();
-            return c;
+            collection.AddTetraPakConfiguration<TetraPakApiConfig>();
+            collection.TryAddSingleton<IClientCredentialsGrantService, TetraPakClientCredentialsGrantService>();
+            return collection;
         }
 
+        /// <summary>
+        ///   Parses and returns the <see cref="TokenExchangeResponse.ExpiresIn"/> value
+        ///   as a <see cref="TimeSpan"/> value.
+        /// </summary>
         public static TimeSpan? GetLifespan(this TokenExchangeResponse self)
         {
             if (string.IsNullOrWhiteSpace(self.ExpiresIn) || !int.TryParse(self.ExpiresIn, out var seconds))
